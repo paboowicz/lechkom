eee13654062951063b5b48934a12a34269625df3
Pabo <paboowicz@gmail.com>
2017-01-30 19:49:59 +0100

    upgrade submit.php

--- assets/php/submit.php
+++ assets/php/submit.php
@@ -1,5 +1,5 @@
 <?php
-$your_email="s.lechowicz@lechkom.pl";
+$your_email="paboowicz@gmail.com";
 
 if(!empty($_POST))
 {
@@ -16,41 +16,4 @@ if(!empty($_POST))
 	mail($to, $subject, $message, $headers);
 }
 
-?>
-<?php
-} else {
-//formularz przesłano
-//wysyłamy więc maila
-
-
-require_once('class.phpmailer.php');
-
-$mail = new PHPMailer();
-
-
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->CharSet = 'utf-8';
-$mail->Host       = "smtp.gmail.com"; // SMTP server
-$mail->From       = "lechkomwww@gmail.com";
-$mail->FromName   = "formularz";
-$mail->SMTPAuth = true;
-$mail->Username = 'lechkomwww@gmail.com';
-$mail->Password = 'elsebek1983	';
-
-//budujemy treśc maila
-$bodyHtml = 'Imie:'.$_POST['name'].'<br />';
-$bodyHtml.= 'email:'.$_POST['email'].'<br />';
-$bodyHtml.= 'Pozostałe dane';
-$mail->Subject    = 'Jakiś temat';
-$mail->AltBody    = strip_tags($bodyHtml); // treść tekstowa
-$mail->MsgHTML($bodyHtml);
-
-//do kogo wysyłamy
-$mail->AddAddress("lechkomwww@gmail.com");
-if(!$mail->Send()) {
-  echo "Blad wysylki: " . $mail->ErrorInfo;
-} else {
-  echo "Formularz wyslano";
-}
-}
 ?>
